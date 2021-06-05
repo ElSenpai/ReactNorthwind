6548fb256ef78b4b884f331d6440202a0688e1e8
El Senpai <El Senpai@EL-SENPAI>
2021-06-06 00:01:19 +0300

    navi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './layouts/Dashboard.jsx';
 function App() {
   return (
     <div className="App">
+      <Navi/>
       <Container className="main">
       <Dashboard/>
       </Container>
