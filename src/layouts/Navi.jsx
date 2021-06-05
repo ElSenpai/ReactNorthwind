9ffba6daaf8d9a0708415e95866d39afc38a4ad8
El Senpai <El Senpai@EL-SENPAI>
2021-06-05 23:32:23 +0300

    menu

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,34 +1,38 @@
 import React from 'react'
-import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import { Container, Button, Dropdown, Menu } from 'semantic-ui-react'
 
 
 export default function Navi() {
     return (
         <div>
-             <Menu inverted >
-        <Menu.Item
-          name='home'
-          
-        />
-        <Menu.Item
-          name='messages'
-          
-        />
+            <Menu inverted >
+            <Container>
+                
+                    <Menu.Item
+                        name='home'
 
-        <Menu.Menu position='right'>
-          <Dropdown item text='Language'>
-            <Dropdown.Menu>
-              <Dropdown.Item>English</Dropdown.Item>
-              <Dropdown.Item>Russian</Dropdown.Item>
-              <Dropdown.Item>Spanish</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+                    />
+                    <Menu.Item
+                        name='messages'
 
-          <Menu.Item>
-            <Button primary>Sign Up</Button>
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+                    />
+
+                    <Menu.Menu position='right'>
+                        <Dropdown item text='Language'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>English</Dropdown.Item>
+                                <Dropdown.Item>Russian</Dropdown.Item>
+                                <Dropdown.Item>Spanish</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+               
+            </Container>
+         </Menu>
         </div>
     )
 }
