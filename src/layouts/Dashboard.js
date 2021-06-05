490d22e4573ba433f97150ae2709b660f11f1792
El Senpai <El Senpai@EL-SENPAI>
2021-06-05 22:55:54 +0300

    hey

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Button } from 'semantic-ui-react'
 export default function Dashboard() {
     return (
         <div>
-            Ana Sayfa
+           <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
         </div>
     )
 }
