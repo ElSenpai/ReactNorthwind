a8a76d97c29de46583a3396f013d8fc5652a731a
El Senpai <El Senpai@EL-SENPAI>
2021-06-05 23:17:44 +0300

    hey

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import CartSummary from './CartSummary'
+
 export default function Navi() {
     return (
         <div>
-            
+            <CartSummary/>
         </div>
     )
 }
